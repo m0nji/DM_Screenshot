afa38bc47980132ab3bfc018cd02ba216fc913c7
Thomas <karl_dall@gmx.de>
2026-06-19 07:25:32 +0200

    build(mac): add Sparkle dependency, embed + sign framework, bundle CHANGELOG

--- mac/Package.swift
+++ mac/Package.swift
@@ -4,9 +4,13 @@ import PackageDescription
 let package = Package(
     name: "DMShot",
     platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/sparkle-project/Sparkle", from: "2.6.0"),
+    ],
     targets: [
         .executableTarget(
             name: "DMShot",
+            dependencies: [.product(name: "Sparkle", package: "Sparkle")],
             path: "Sources/DMShot",
             swiftSettings: [.swiftLanguageMode(.v5)]
         ),
